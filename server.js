ff82e01794576dcec00521a4a6cf765a97df578e
Joey Sutcliffe <joeysutcliffe01@gmail.com>
2024-06-14 15:15:07 +0200

    Updated api

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3500;
 
-app.use(cors({ origin: "https://country-wiki-n98f.onrender.com" }));
+app.use(cors({ origin: "https://country-wiki-n98f.onrender.com"  }));
+// app.use(cors({ origin: "http://localhost:3000" }));
 
 app.use(express.json());
 
@@ -11,7 +12,15 @@ app.post("/country", async (req, res) => {
   const { userInput, country } = req.body;
   const input = userInput ? userInput : country;
 
+  console.log("req;", req);
+
   try {
+
+    // They may have shut down this api
+    // const restcountriesApi = `https://restfulcountries.com/api/v1/countries/${input}`;
+
+    console.log("Inside of try fetch");
+
     const restcountriesApi = `https://restcountries.com/v3.1/name/${input}?fullText=true`;
 
     const response = await fetch(restcountriesApi);
@@ -20,7 +29,7 @@ app.post("/country", async (req, res) => {
     res.json(data);
   } catch (err) {
     console.log(err);
-    res.status(400).json(err);
+    res.status(400).json("Error message inside of backend fetch",err);
   }
 });
 
